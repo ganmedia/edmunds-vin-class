387cadc2817df5d824e8e67f2998988e69e8b11e
Daian Gan <dgfotografias@gmail.com>
2014-09-09 23:09:11 -0400

    adding new charatteristics to the vin class

--- vin_class.php
+++ vin_class.php
@@ -10,6 +10,16 @@ class Edmunds_vin_decorer {
 		$this->base_json_path = $this->data['styleHolder'][0];
 	}
 
+	public function OfficialName() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		$name = array();
+		$name_array = $this->base_json_path['attributeGroups']['MAIN']['attributes'];
+		foreach ($name_array as $key=>$value) {
+			if ($value['name'] == 'NAME') {
+				$name[$value['name']] = $value['value'];
+			}
+		}
+		return $name;
+	}
 	public function Make() { # OUTPUT EXAMPLE: Toyota
 		return $this->base_json_path['makeName'];
 	}
@@ -24,6 +34,9 @@ class Edmunds_vin_decorer {
 	}
 	public function EngineType() { # OUTPUT EXAMPLE: Gas
 		return $this->base_json_path['engineType'];
+	}
+	public function NumberOfDoors() { # OUTPUT EXAMPLE: A number, for example: 4
+		return $this->base_json_path['attributeGroups']['DOORS']['attributes']['NUMBER_OF_DOORS']['value'];
 	}
 	public function EngineFuelType() { # OUTPUT EXAMPLE: premium unleaded (required)
 		return $this->base_json_path['engineFuelType'];
@@ -47,67 +60,70 @@ class Edmunds_vin_decorer {
 		return $this->base_json_path['categories']['Market'];
 	}
 	public function AirConditioning() { # OUTPUT EXAMPLE: Array(REAR_HEAT=>rear ventilation ducts, AIR_FILTRATION=>interior air filtration) 
-		$air = array();
-		$air_array = $this->base_json_path['attributeGroups']['AIR_CONDITIONING']['attributes'];
-		foreach ($air_array as $key=>$value) {
-			$air[$value['name']] = $value['value'];
-		}
-		return $air;
+		return $this->characteristics_list('AIR_CONDITIONING');
 	}
 	public function MobileConnectivity() { # OUTPUT EXAMPLE: Array(PHONE=>pre-wired for phone, BLUETOOTH=>Bluetooth) 
-		$mobile = array();
-		$mobile_array = $this->base_json_path['attributeGroups']['MOBILE_CONNECTIVITY']['attributes'];
-		foreach ($mobile_array as $key=>$value) {
-			$mobile[$value['name']] = $value['value'];
-		}
-		return $mobile;
-	}
-	public function NumberOfDoors() { # OUTPUT EXAMPLE: A number, for example: 4
-		return $this->base_json_path['attributeGroups']['DOORS']['attributes']['NUMBER_OF_DOORS']['value'];
+		return $this->characteristics_list('MOBILE_CONNECTIVITY');
 	}
 	public function Airbags() { # OUTPUT EXAMPLE: Array(HEAD_AIRBAGS=>front and rear, SIDE_AIRBAGS=>dual front)
-		$airbags = array();
-		$airbags_array = $this->base_json_path['attributeGroups']['AIRBAGS']['attributes'];
-		foreach ($airbags_array as $key=>$value) {
-			$airbags[$value['name']] = $value['value'];
-		}
-		return $airbags;
+		return $this->characteristics_list('AIRBAGS');
 	}
 	public function ExteriorLights() { # OUTPUT EXAMPLE: Front Fog Lights, Xenon Headlights
-		$lights_array = $this->base_json_path['attributeGroups']['EXTERIOR_LIGHTS']['attributes'];
-		$lights = array();
-		foreach ($lights_array as $key => $value) {
-			$lights[] = $key;
-		}
-		return $lights;
+		return $this->characteristics_list('EXTERIOR_LIGHTS');
 	}
 	public function ExteriorDimensions() { # OUTPUT EXAMPLE: Array(WHEELBASE=>109.3, OVERALL_LENGTH=>193.4)
-		$dimensions = array();
-		$dimensions_array = $this->base_json_path['attributeGroups']['EXTERIOR_DIMENSIONS']['attributes'];
-		foreach ($dimensions_array as $key=>$value) {
-			$dimensions[$value['name']] = $value['value'];
-		}
-		return $dimensions;
+		return $this->characteristics_list('EXTERIOR_DIMENSIONS');
 	}
 	public function ConsumptionSpecification() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
-		$consumption = array();
-		$consumption_array = $this->base_json_path['attributeGroups']['SPECIFICATIONS']['attributes'];
-		foreach ($consumption_array as $key=>$value) {
-			$consumption[$value['name']] = $value['value'];
-		}
-		return $consumption;
+		return $this->characteristics_list('SPECIFICATIONS');
 	}
+	public function BrakeSystem() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics_list('BRAKE_SYSTEM');
+	}
+	public function CrashTestRatings() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics_list('CRASH_TEST_RATINGS');
+	}
+	public function Mirrors() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics_list('MIRRORS');
+	}
+	public function InteriorTrim() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics_list('INTERIOR_TRIM');
+	}
+	public function SteeringWheel() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics_list('STEERING_WHEEL');
+	}
+	public function ChildSafety() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+		return $this->characteristics('CHILD_SAFETY');
+	}
+
+	public function characteristics($attribute) {
+		$characteristics = array();
+		$characteristics_array = $this->base_json_path['attributeGroups'][$attribute]['attributes'];
+		foreach ($characteristics_array as $key=>$value) {
+			$characteristics[] = $key;
+		}
+		return $characteristics;
+	}
+	public function characteristics_list($attribute) {
+		$characteristics = array();
+		$characteristics_array = $this->base_json_path['attributeGroups'][$attribute]['attributes'];
+		foreach ($characteristics_array as $key=>$value) {
+			$characteristics[$value['name']] = $value['value'];
+		}
+		return $characteristics;
+	}
+
 } # CLASS END
 
-$my_car = new Edmunds_vin_decorer('1FAFP52U9XA140531');
+$my_car = new Edmunds_vin_decorer('19UUA96249A800952');
 
-print_r($my_car->ConsumptionSpecification());
+print_r($my_car->ChildSafety());
 echo '<br>';
-// foreach ($my_car->ExteriorLights() as $item) {
-// 	echo $item.'<br>';
-// }
-foreach ($my_car->ConsumptionSpecification() as $key=>$value) {
-	echo $key . ': ' . $value . '<br>';
+foreach ($my_car->ChildSafety() as $item) {
+	echo $item.'<br>';
 }
+// foreach ($my_car->ChildSafety() as $key=>$value) {
+// 	echo $key . ': ' . $value . '<br>';
+// }
 
 ?>
