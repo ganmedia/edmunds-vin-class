31bec6e6214db8e66639856763bea26f404b5e2a
Daian Gan <dgfotografias@gmail.com>
2014-09-09 20:03:37 -0400

    Adding class update

--- vin_class.php
+++ vin_class.php
@@ -1,8 +1,6 @@
 
 <?php
 
-//include 'functions.php';
-
 class Edmunds_vin_decorer {
 
 	public function __construct($vin, $api_key = 'dc9a9ffxmhbdbcqhv2h5d6ez') {
@@ -12,43 +10,43 @@ class Edmunds_vin_decorer {
 		$this->base_json_path = $this->data['styleHolder'][0];
 	}
 
-	public function Make() { #Toyota
+	public function Make() { # OUTPUT EXAMPLE: Toyota
 		return $this->base_json_path['makeName'];
 	}
-	public function Model() { #Corolla
+	public function Model() { # OUTPUT EXAMPLE: Corolla
 		return $this->base_json_path['modelName'];
 	}
-	public function Year() { #2013
+	public function Year() { # OUTPUT EXAMPLE: 2013
 		return $this->base_json_path['year'];
 	}
-	public function TransmissionType() { #Automatic
+	public function TransmissionType() { # OUTPUT EXAMPLE: Automatic
 		return $this->base_json_path['transmissionType'];
 	}
-	public function EngineType() { #Gas
+	public function EngineType() { # OUTPUT EXAMPLE: Gas
 		return $this->base_json_path['engineType'];
 	}
-	public function EngineFuelType() { #premium unleaded (required)
+	public function EngineFuelType() { # OUTPUT EXAMPLE: premium unleaded (required)
 		return $this->base_json_path['engineFuelType'];
 	}
-	public function EngineCylinder() { #6
+	public function EngineCylinder() { # OUTPUT EXAMPLE: 6
 		return $this->base_json_path['engineCylinder'];
 	}
-	public function EngineSize() { #3.7
+	public function EngineSize() { # OUTPUT EXAMPLE: 3.7
 		return $this->base_json_path['engineSize'];
 	}
-	public function VehicleType() { #Car
+	public function VehicleType() { # OUTPUT EXAMPLE: Car
 		return $this->base_json_path['categories']['PRIMARY_BODY_TYPE'][0];
 	}
-	public function VehicleStyle() { #Sedan
+	public function VehicleStyle() { # OUTPUT EXAMPLE: Sedan
 		return $this->base_json_path['categories']['Vehicle Style'][0];
 	}
-	public function VehicleSize() { #Midsize
+	public function VehicleSize() { # OUTPUT EXAMPLE: Midsize
 		return $this->base_json_path['categories']['Vehicle Size'][0];
 	}
-	public function Market() { #Simple Array(Luxury, Performance) 
+	public function Market() { # OUTPUT EXAMPLE: Simple Array(Luxury, Performance) 
 		return $this->base_json_path['categories']['Market'];
 	}
-	public function AirConditioning() { #Array(REAR_HEAT=>rear ventilation ducts, AIR_FILTRATION=>interior air filtration) 
+	public function AirConditioning() { # OUTPUT EXAMPLE: Array(REAR_HEAT=>rear ventilation ducts, AIR_FILTRATION=>interior air filtration) 
 		$air = array();
 		$air_array = $this->base_json_path['attributeGroups']['AIR_CONDITIONING']['attributes'];
 		foreach ($air_array as $key=>$value) {
@@ -56,7 +54,7 @@ class Edmunds_vin_decorer {
 		}
 		return $air;
 	}
-	public function MobileConnectivity() { #Array(PHONE=>pre-wired for phone, BLUETOOTH=>Bluetooth) 
+	public function MobileConnectivity() { # OUTPUT EXAMPLE: Array(PHONE=>pre-wired for phone, BLUETOOTH=>Bluetooth) 
 		$mobile = array();
 		$mobile_array = $this->base_json_path['attributeGroups']['MOBILE_CONNECTIVITY']['attributes'];
 		foreach ($mobile_array as $key=>$value) {
@@ -64,10 +62,10 @@ class Edmunds_vin_decorer {
 		}
 		return $mobile;
 	}
-	public function NumberOfDoors() { #A number, for example: 4
+	public function NumberOfDoors() { # OUTPUT EXAMPLE: A number, for example: 4
 		return $this->base_json_path['attributeGroups']['DOORS']['attributes']['NUMBER_OF_DOORS']['value'];
 	}
-	public function Airbags() { #Array(HEAD_AIRBAGS=>front and rear, SIDE_AIRBAGS=>dual front)
+	public function Airbags() { # OUTPUT EXAMPLE: Array(HEAD_AIRBAGS=>front and rear, SIDE_AIRBAGS=>dual front)
 		$airbags = array();
 		$airbags_array = $this->base_json_path['attributeGroups']['AIRBAGS']['attributes'];
 		foreach ($airbags_array as $key=>$value) {
@@ -75,7 +73,7 @@ class Edmunds_vin_decorer {
 		}
 		return $airbags;
 	}
-	public function ExteriorLights() { #Front Fog Lights, Xenon Headlights
+	public function ExteriorLights() { # OUTPUT EXAMPLE: Front Fog Lights, Xenon Headlights
 		$lights_array = $this->base_json_path['attributeGroups']['EXTERIOR_LIGHTS']['attributes'];
 		$lights = array();
 		foreach ($lights_array as $key => $value) {
@@ -83,7 +81,7 @@ class Edmunds_vin_decorer {
 		}
 		return $lights;
 	}
-	public function ExteriorDimensions() { #Array(WHEELBASE=>109.3, OVERALL_LENGTH=>193.4)
+	public function ExteriorDimensions() { # OUTPUT EXAMPLE: Array(WHEELBASE=>109.3, OVERALL_LENGTH=>193.4)
 		$dimensions = array();
 		$dimensions_array = $this->base_json_path['attributeGroups']['EXTERIOR_DIMENSIONS']['attributes'];
 		foreach ($dimensions_array as $key=>$value) {
@@ -91,7 +89,7 @@ class Edmunds_vin_decorer {
 		}
 		return $dimensions;
 	}
-	public function ConsumptionSpecification() { #Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
+	public function ConsumptionSpecification() { # OUTPUT EXAMPLE: Array(EGE_HIGHWAY_MPG=>25, EPA_CITY_MPG=>17)
 		$consumption = array();
 		$consumption_array = $this->base_json_path['attributeGroups']['SPECIFICATIONS']['attributes'];
 		foreach ($consumption_array as $key=>$value) {
@@ -99,11 +97,9 @@ class Edmunds_vin_decorer {
 		}
 		return $consumption;
 	}
-
-
 } # CLASS END
 
-$my_car = new Edmunds_vin_decorer('19UUA96249A800952');
+$my_car = new Edmunds_vin_decorer('1FAFP52U9XA140531');
 
 print_r($my_car->ConsumptionSpecification());
 echo '<br>';
